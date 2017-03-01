cf85ae78ae5f5fdb669247ba7d980aebb3f0cb74
Igor Camilo <igor.rcamilo@gmail.com>
2017-03-01 15:05:41 -0300

    iOS model target added.

---
+++ PhotoHubKit-iOS/PhotoHubKit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  PhotoHubKit-iOS.h
+//  PhotoHubKit-iOS
+//
+//  Created by Igor Camilo on 01/03/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoHubKit-iOS.
+FOUNDATION_EXPORT double PhotoHubKit_iOSVersionNumber;
+
+//! Project version string for PhotoHubKit-iOS.
+FOUNDATION_EXPORT const unsigned char PhotoHubKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoHubKit_iOS/PublicHeader.h>
+
+
